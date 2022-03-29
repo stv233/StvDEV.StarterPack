3f8988b62400907655585dc2c7cf54226c0ee1cf
Mihkail Glazatov <glazatov.mihail@gmail.com>
2022-03-29 16:31:28 +0700

    GUIMenu update

--- Runtime/StvDEV/StarterPack/Scripts/GUIManager.cs
+++ Runtime/StvDEV/StarterPack/Scripts/GUIManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace StvDEV.StarterPack
@@ -18,35 +19,135 @@ namespace StvDEV.StarterPack
         /// <summary>
         /// Open the menu.
         /// </summary>
-        /// <param name="menu">Menu name</param>
-        public void OpenMenu(string menu)
+        /// <param name="menuName">Menu name</param>
+        public void OpenMenu(string menuName)
         {
-            if (MenuExist(menu))
+            if (TryGetMenu(menuName, out GUIMenu menu))
             {
-                menus[menu].Open();
+                menu.Open();    
+            }
+        }
+
+        /// <summary>
+        /// Opens the menu by its type.
+        /// </summary>
+        /// <typeparam name="T">Menu type</typeparam>
+        public void OpenMenu<T>() where T : GUIMenu
+        {
+            if (TryGetMenu<T>(out GUIMenu menu))
+            {
+                menu.Open();
             }
         }
 
         /// <summary>
         /// Close the menu.
         /// </summary>
-        /// <param name="menu">Menu name</param>
-        public void CloseMenu(string menu)
+        /// <param name="menuName">Menu name</param>
+        public void CloseMenu(string menuName)
         {
-            if (MenuExist(menu))
+            if (TryGetMenu(menuName, out GUIMenu menu))
             {
-                menus[menu].Close();
+                menu.Close();
             }
+        }
+
+        /// <summary>
+        /// Closes the menu by its type.
+        /// </summary>
+        /// <typeparam name="T">Menu type</typeparam>
+        public void CloseMenu<T>() where T: GUIMenu
+        {
+            if (TryGetMenu<T>(out GUIMenu menu))
+            {
+                menu.Open();
+            }
+        }
+
+        /// <summary>
+        /// Returns the menu by its name
+        /// </summary>
+        /// <param name="menuName">Menu name</param>
+        /// <returns>Menu</returns>
+        public GUIMenu GetMenu(string menuName)
+        {
+            if (MenuExist(menuName))
+            {
+                return menus[menuName];
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// Tries to get a menu by its name.
+        /// </summary>
+        /// <param name="menuName">Menu name</param>
+        /// <param name="menu">Result</param>
+        /// <returns>Success</returns>
+        public bool TryGetMenu(string menuName, out GUIMenu menu)
+        {
+            menu = GetMenu(menuName);
+            if (menu != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Gets the menu by its type.
+        /// </summary>
+        /// <typeparam name="T">Menu type</typeparam>
+        /// <returns>Menu</returns>
+        public GUIMenu GetMenu<T>() where T: GUIMenu
+        {
+            return menus.Values.Where(x => x.GetType() == typeof(T)).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Tries to get a menu by its type.
+        /// </summary>
+        /// <typeparam name="T">Menu type</typeparam>
+        /// <param name="menu">Result</param>
+        /// <returns>Success</returns>
+        public bool TryGetMenu<T>(out GUIMenu menu) where T: GUIMenu
+        {
+            menu = GetMenu<T>();
+            if (menu != null)
+            {
+                return true;
+            }
+            return false;
         }
 
         /// <summary>
         /// Checks the menu for existence.
         /// </summary>
-        /// <param name="menu">Menu name</param>
+        /// <param name="menuName">Menu name</param>
         /// <returns>Existence</returns>
-        public bool MenuExist(string menu)
+        public bool MenuExist(string menuName)
         {
-            return menus.ContainsKey(menu);
+            return menus.ContainsKey(menuName);
+        }
+
+        /// <summary>
+        /// Checks the menu for existence.
+        /// </summary>
+        /// <typeparam name="T">Menu type</typeparam>
+        /// <returns>Existence</returns>
+        public bool MenuExist<T>() where T : GUIMenu
+        {
+            return TryGetMenu<T>(out GUIMenu menu);
+        }
+
+        /// <summary>
+        /// Registers the menu in the manager.
+        /// </summary>
+        /// <param name="menu">Menu</param>
+        public void RegisterMenu(GUIMenu menu)
+        {
+            menus[menu.gameObject.name] = menu;
         }
 
         /// <summary>
@@ -56,7 +157,7 @@ namespace StvDEV.StarterPack
         {
             foreach(GUIMenu menu in GameObject.FindObjectsOfType<GUIMenu>())
             {
-                menus[menu.gameObject.name] = menu;
+                RegisterMenu(menu);
             }
         }
     }
--- Runtime/StvDEV/StarterPack/Scripts/GUIMenu.cs
+++ Runtime/StvDEV/StarterPack/Scripts/GUIMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace StvDEV.StarterPack
@@ -8,11 +9,28 @@ namespace StvDEV.StarterPack
     public class GUIMenu : MonoBehaviour 
     {
         /// <summary>
+        /// Called when the menu is opened.
+        /// </summary>
+        public event Action OnOpen;
+
+        /// <summary>
+        /// Called when the menu is closed.
+        /// </summary>
+        public event Action OnClose;
+
+        /// <summary>
+        /// Returns whether this menu is active.
+        /// </summary>
+        public bool IsActive => gameObject.activeInHierarchy;
+
+        /// <summary>
         /// Open menu.
         /// </summary>
         public void Open()
         {
             gameObject.SetActive(true);
+            OnOpened();
+            OnOpen?.Invoke();
         }
 
         /// <summary>
@@ -21,6 +39,18 @@ namespace StvDEV.StarterPack
         public void Close()
         {
             gameObject.SetActive(false);
+            OnClosed();
+            OnClose?.Invoke();
         }
+
+        /// <summary>
+        /// Defines the actions that occur when the window is opened.
+        /// </summary>
+        protected virtual void OnOpened() { }
+
+        /// <summary>
+        /// Defines the actions that occur when the window is closed.
+        /// </summary>
+        protected virtual void OnClosed() { }
     }
 }
