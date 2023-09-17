cbd1ae1094a7488c39094cf8537b347d7479c888
Mikhail Glazatov <glazatov.mihail@gmail.com>
2023-09-17 23:15:27 +0700

    Add dialogs

---
+++ Runtime/Components/UI/Dialogs/Dialog.cs
@@ -0,0 +1,76 @@
+using StvDEV.Patterns;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StvDEV.Components.UI.Dialogs
+{
+    /// <summary>
+    /// Dialog results.
+    /// </summary>
+    public enum DialogResult
+    {
+        None,
+        Ok,
+        Yes,
+        No,
+        Cancel,
+    }
+
+    /// <summary>
+    /// Dialog basic class.
+    /// </summary>
+    /// <typeparam name="T">Dialog singleton.</typeparam>
+    public class Dialog<T> : MonoBehaviourSingleton<T> where T : Dialog<T>
+    {
+        private Action<DialogResult> _callback;
+
+        protected override void AwakeSingletone()
+        {
+            Hide(DialogResult.None);
+            base.AwakeSingletone();
+        }
+
+        /// <summary>
+        /// Show dialog.
+        /// </summary>
+        /// <param name="callback">Dialog callback</param>
+        protected void Show(Action<DialogResult> callback)
+        {
+            _callback = callback;
+            gameObject.SetActive(true);
+        }
+
+        /// <summary>
+        /// Hide dialog.
+        /// </summary>
+        /// <param name="result">Dialog result</param>
+        protected void Hide(DialogResult result)
+        {
+            Action<DialogResult> callback = _callback;
+            _callback = null;
+            gameObject.SetActive(false);
+            callback?.Invoke(result);
+        }
+
+        /// <summary>
+        /// Open dialog window.
+        /// </summary>
+        /// <param name="callback">Dialog callback</param>
+        public static void ShowDialog(Action<DialogResult> callback)
+        {
+            Instance.Show(callback);
+        }
+
+        /// <summary>
+        /// Close dialog window.
+        /// </summary>
+        /// <param name="result">Dialog result</param>
+        public static void Close(DialogResult result)
+        {
+            Instance.Hide(result);
+        }
+
+    }
+}
---
+++ Runtime/Components/UI/Dialogs/MessageBox.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace StvDEV.Components.UI.Dialogs
+{
+
+    /// <summary>
+    /// Gloabal message box.
+    /// </summary>
+    public class MessageBox : Dialog<MessageBox>
+    {
+        /// <summary>
+        /// Buttons for message box.
+        /// </summary>
+        public enum MessageBoxButtons
+        {
+            Ok,
+            YesNo,
+            YesNoCancel,
+        }
+
+        [Header("Text")]
+        [SerializeField] private TMP_Text _text;
+        
+        [Header("Buttons")]
+        [Header("Ok")]
+        [SerializeField] private Button _ok;
+
+        [Header("Yes/No")]
+        [SerializeField] private Button _yes;
+        [SerializeField] private Button _no;
+
+        [Header("Cancel")]
+        [SerializeField] private Button _cancel;
+
+        /// <summary>
+        /// Current message.
+        /// </summary>
+        public string Message => _text.text;
+
+        protected override void AwakeSingletone()
+        {
+            _ok.onClick.AddListener(() =>
+            {
+                Hide(DialogResult.Ok);
+            });
+            _yes.onClick.AddListener(() =>
+            {
+                Hide(DialogResult.Yes);
+            });
+            _no.onClick.AddListener(() =>
+            {
+                Hide(DialogResult.No);
+            });
+            _cancel.onClick.AddListener(() =>
+            {
+                Hide(DialogResult.Cancel);
+            });
+            Close();
+            base.AwakeSingletone();
+        }
+
+        private void ShowButtons(MessageBoxButtons buttons)
+        {
+            HideAllButtons();
+            switch (buttons)
+            {
+                case MessageBoxButtons.YesNo:
+                    _yes.gameObject.SetActive(true);
+                    _no.gameObject.SetActive(true);
+                    break;
+                case MessageBoxButtons.YesNoCancel:
+                    _yes.gameObject.SetActive(true);
+                    _no.gameObject.SetActive(true);
+                    _cancel.gameObject.SetActive(true);
+                    break;
+                default:
+                    _ok.gameObject.SetActive(true);
+                    break;
+            }
+        }
+
+        private void HideAllButtons()
+        {
+            _ok.gameObject.SetActive(false);
+            _yes.gameObject.SetActive(false);
+            _no.gameObject.SetActive(false);
+        }
+
+        /// <summary>
+        /// Show message box with spesifed message.
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <param name="buttons">Buttons</param>
+        public static void Show(string message, MessageBoxButtons buttons = MessageBoxButtons.Ok)
+        {
+            ShowDialog(message, buttons, null);
+        }
+
+        /// <summary>
+        /// Show message box with callback after closed.
+        /// </summary>
+        /// <param name="message">Message</param>
+        /// <param name="buttons">Buttons</param>
+        /// <param name="callback">Dialog close callback</param>
+        public static void ShowDialog(string message, MessageBoxButtons buttons = MessageBoxButtons.Ok, Action<DialogResult> callback = null)
+        {
+            Instance.ShowButtons(buttons);
+            Instance._text.text = message;
+            ShowDialog(callback);
+        }
+
+        /// <summary>
+        /// Hide dialog.
+        /// </summary>
+        /// <param name="dialogResult">Result from which the dialog was closed</param>
+        public new static void Close(DialogResult dialogResult = DialogResult.No)
+        {
+            Instance.Hide(dialogResult);
+        }
+
+    }
+}
