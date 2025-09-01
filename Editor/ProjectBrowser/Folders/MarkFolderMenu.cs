990ab71bfefddcecc75573da420850f4e7bd3546
Mikhail Glazatov <glazatov.mihail@gmail.com>
2025-09-01 14:45:03 +0700

    [Fix] Icon menu priority

--- Editor/ProjectBrowser/Folders/MarkFolderMenu.cs
+++ Editor/ProjectBrowser/Folders/MarkFolderMenu.cs
@@ -11,7 +11,7 @@ namespace StvDEV.ProjectBrowser.Folders
     {
         private const string PARENT_MENU = "Assets/Mark Folder As";
 
-        [MenuItem(PARENT_MENU + "/Default", priority = -12)]
+        [MenuItem(PARENT_MENU + "/Default")]
         private static void MarkAsDefault()
         {
             RemoveIconFromFolder(Selection.assetGUIDs[0]);
@@ -23,109 +23,109 @@ namespace StvDEV.ProjectBrowser.Folders
             return Selection.assetGUIDs.Length == 1 && ObjectIsFolder(Selection.activeObject) && FolderHasIcon(Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Animations")]
+        [MenuItem(PARENT_MENU + "/Animations", priority = 1015)]
         private static void MarkAsAnimations()
         {
             AddIconToFolder("Animations", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Audio")]
+        [MenuItem(PARENT_MENU + "/Audio", priority = 1015)]
         private static void MarkAsAudio()
         {
             AddIconToFolder("Audio", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Editor")]
+        [MenuItem(PARENT_MENU + "/Editor", priority = 1015)]
         private static void MarkAsEditor()
         {
             AddIconToFolder("Editor", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Fonts")]
+        [MenuItem(PARENT_MENU + "/Fonts", priority = 1015)]
         private static void MarkAsFonts()
         {
             AddIconToFolder("Fonts", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Home")]
+        [MenuItem(PARENT_MENU + "/Home", priority = 1015)]
         private static void MarkAsHome()
         {
             AddIconToFolder("Home", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Materials")]
+        [MenuItem(PARENT_MENU + "/Materials", priority = 1015)]
         private static void MarkAsMaterials()
         {
             AddIconToFolder("Materials", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Models")]
+        [MenuItem(PARENT_MENU + "/Models", priority = 1015)]
         private static void MarkAsModels()
         {
             AddIconToFolder("Models", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Plugins")]
+        [MenuItem(PARENT_MENU + "/Plugins", priority = 1015)]
         private static void MarkAsPlugins()
         {
             AddIconToFolder("Plugins", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Prefabs")]
+        [MenuItem(PARENT_MENU + "/Prefabs", priority = 1015)]
         private static void MarkAsPrefabs()
         {
             AddIconToFolder("Prefabs", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Presets")]
+        [MenuItem(PARENT_MENU + "/Presets", priority = 1015)]
         private static void MarkAsPresets()
         {
             AddIconToFolder("Presets", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Resources")]
+        [MenuItem(PARENT_MENU + "/Resources", priority = 1015)]
         private static void MarkAsResources()
         {
             AddIconToFolder("Resources", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Runtime")]
+        [MenuItem(PARENT_MENU + "/Runtime", priority = 1015)]
         private static void MarkAsRuntime()
         {
             AddIconToFolder("Runtime", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Scenes")]
+        [MenuItem(PARENT_MENU + "/Scenes", priority = 1015)]
         private static void MarkAsScenes()
         {
             AddIconToFolder("Scenes", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Scripts")]
+        [MenuItem(PARENT_MENU + "/Scripts", priority = 1015)]
         private static void MarkAsScripts()
         {
             AddIconToFolder("Scripts", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Settings")]
+        [MenuItem(PARENT_MENU + "/Settings", priority = 1015)]
         private static void MarkAsSettings()
         {
             AddIconToFolder("Settings", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Shaders")]
+        [MenuItem(PARENT_MENU + "/Shaders", priority = 1015)]
         private static void MarkAsShaders()
         {
             AddIconToFolder("Shaders", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Sprites")]
+        [MenuItem(PARENT_MENU + "/Sprites", priority = 1015)]
         private static void MarkAsSprites()
         {
             AddIconToFolder("Sprites", Selection.assetGUIDs[0]);
         }
 
-        [MenuItem(PARENT_MENU + "/Textures")]
+        [MenuItem(PARENT_MENU + "/Textures", priority = 1015)]
         private static void MarkAsTextures()
         {
             AddIconToFolder("Textures", Selection.assetGUIDs[0]);
